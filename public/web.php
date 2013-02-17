e1e1e05084b6b0908b3d0ddddbb2d73c9c4d329f
Richard McIntyre <richard.mackstar@gmail.com>
2013-02-17 09:52:25 +0900

    Added documentation to public/web.php

--- public/web.php
+++ public/web.php
@@ -2,6 +2,10 @@
 /**
  * CLI / Built-in web server script for development
  *
+ * This script is the entry point for CLI an application whilst in development. When in production look
+ * at index.php. This script is a base guideline and this procedural boot strap is gives you some defaults
+ * as a guide. You are free to change and configure this script at will.
+ *
  * CLI:
  * $ php web.php get /
  * $ php web.php get / prod
@@ -14,22 +18,39 @@
  * @global  $mode string
  */
 
-// Get application instance with $mode (Prod, Dev, Api, Stub, Test)
+/**
+ * Here we get an application instance by setting a $mode variable such as (Prod, Dev, Api, Stub, Test)
+ * the dev instance provides debugging tools and defaults to help you the development of your application.
+ */
 $mode = 'Dev';
 $app = require dirname(__DIR__) . '/scripts/bootstrap/dev_instance.php';
 
-// Cleaning (comment out to enable cache)
+/**
+ * The cache is cleared on each request via the following script. We understand that you may want to debug
+ * your application with caching turned on. When doing so just comment out the following.
+ */
 require  dirname(__DIR__) . '/scripts/clear.php';
 
-// Return if direct file access in built-in web server
+/**
+ * When using the built in file-server when directly accessing files the app instance will not be created and
+ * and the script will be exited.
+ *
+ * @var $app \BEAR\Package\Provide\Application\AbstractApp
+ */
 if (! $app) {
     return false;
 }
-/** @var $app \BEAR\Package\Provide\Application\AbstractApp */
 
-// Dispatch
+/**
+ * Calling the match of a Bear.Sunday compatible router will give us the $method, $pagePath, $query to be used
+ * in the page request.
+ */
 list($method, $pagePath, $query) = $app->router->match();
 
+/**
+ * An attempt to request the page resource is made along with setting the response with the resource itself.
+ * Upon failure the exception handler will be triggered.
+ */
 try {
     // Request
     $app->page = $app->resource->$method->uri('page://self/' . $pagePath)->withQuery($query)->eager->request();

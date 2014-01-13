bcedb00b6034c0c18e2e6a5909a03acd3f42ddf7
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-01-13 11:11:09 +0900

    test bootstrap cleaning

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,18 +6,14 @@ use BEAR\Skeleton\Module\AppModule;
 
 error_reporting(E_ALL);
 
-// set application root as current directory
-chdir(dirname(__DIR__));
-
 // load
-$loader = require_once 'bootstrap/autoload.php';
+$loader = require_once dirname(__DIR__) . '/bootstrap/autoload.php';
 
 // enable debug print p($var);
 (new Dev())->loadDevFunctions();
 
 // set the application path into the globals so we can access it in the tests.
 $GLOBALS['APP_DIR'] = dirname(__DIR__);
-
 
 // set the resource client
 $GLOBALS['RESOURCE'] = Injector::create([new AppModule('test')])->getInstance('\BEAR\Resource\ResourceInterface');

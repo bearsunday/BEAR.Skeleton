414130c155e8ffe5a890b8b8177867465c8b5861
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-01-13 10:42:33 +0900

    remove /var/www/dev. admin

--- var/www/dev/resource/graph.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * resource graph
- *
- * @global $app \BEAR\Package\Provide\Application\AbstractApp
- */
-
-$resourceObject = $app->resource->newInstance($_GET['uri']);
-print_o($resourceObject);

35e7b878756fe2b222e013226c008c9ee804bc46
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-01-13 11:10:06 +0900

    test coverage 100%

--- tests/Module/AppModuleTest.php
+++ tests/Module/AppModuleTest.php
@@ -2,6 +2,8 @@
 
 namespace BEAR\Skeleton\Module;
 
+use Ray\Di\Injector;
+
 class AppModuleTest extends \PHPUnit_Framework_TestCase
 {
     /**
@@ -9,14 +11,43 @@ class AppModuleTest extends \PHPUnit_Framework_TestCase
      */
     private $module;
 
+    /**
+     * @var Injector
+     */
+    private $injector;
+
     protected function setUp()
     {
-        $this->module = new AppModule('prod');
+        static $module;
+        static $injector;
+
+        if (! $module) {
+            $module = new AppModule('prod');
+            $module->activate();
+        }
+        if (! $injector) {
+            $injector = Injector::create([$module]);
+        }
+        $this->module = clone $module;
+        $this->injector = clone $injector;
         parent::setUp();
     }
 
     public function testNew()
     {
         $this->assertInstanceOf('BEAR\Skeleton\Module\AppModule', $this->module);
+    }
+
+    public function testApiContext()
+    {
+        $this->injector->setModule(new AppModule('api'));
+        $actual = $this->injector->getInstance('BEAR\Resource\RenderInterface');
+        $this->assertInstanceOf('BEAR\Package\Provide\ResourceView\HalRenderer', $actual);
+    }
+
+    public function testBindApp()
+    {
+        $actual = $this->injector->getInstance('BEAR\Sunday\Extension\Application\AppInterface');
+        $this->assertInstanceOf('BEAR\Skeleton\App', $actual);
     }
 }

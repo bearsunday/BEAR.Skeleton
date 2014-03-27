33e635fee2d9ffa6ef3bb70ea8c69bf0654e74c0
koriym <akihito.koriyama@gmail.com>
2014-03-27 13:41:31 +0900

    bootstrap function to Bootstrap class

--- bin/clear.php
+++ bin/clear.php
@@ -2,28 +2,15 @@
 /**
  * Application clear script
  *
- * Clear:
- *  apc code cache
- *  apc user cache
- *  smarty compile script
- *  tmp files
  */
 
-// APC Cache
-if (function_exists('apc_clear_cache')) {
-    if (version_compare(phpversion('apc'), '4.0.0') < 0) {
-        apc_clear_cache('user');
-    }
-    apc_clear_cache();
-}
+use BEAR\Package\Bootstrap\Bootstrap;
 
-$unlink = function ($path) use (&$unlink) {
-    foreach (glob(rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . '*') as $file) {
-        is_dir($file) ? $unlink($file) : unlink($file);
-        @rmdir($file);
-    }
-};
+require dirname(__DIR__) . '/bootstrap/autoload.php';
 
-$unlink(dirname(__DIR__) . '/var/tmp');
+$clearDirs = [
+    dirname(__DIR__) . '/var/tmp'
+];
 
-unset($unlink);
+Bootstrap::clearApp($clearDirs);
+

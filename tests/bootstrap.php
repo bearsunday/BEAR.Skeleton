8819c137faa37e2e392dc1e596eb21feb53db3fc
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-05-12 20:29:34 +0900

    update tests/bootstrap.php
    clear tmp files
    use $_ENV

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,13 +7,15 @@ use BEAR\Skeleton\Module\AppModule;
 error_reporting(E_ALL);
 
 // load
-$loader = require_once dirname(__DIR__) . '/bootstrap/autoload.php';
-
-// enable debug print p($var);
-(new Dev())->loadDevFunctions();
+$loader = require dirname(__DIR__) . '/vendor/autoload.php';
+/** @var $loader \Composer\Autoload\ClassLoader */
+$loader->addPsr4('BEAR\Skeleton\\', __DIR__);
 
 // set the application path into the globals so we can access it in the tests.
-$GLOBALS['APP_DIR'] = dirname(__DIR__);
+$_ENV['APP_DIR'] = dirname(__DIR__);
+
+// clear cache files
+require $_ENV['APP_DIR'] . '/bin/clear.php';
 
 // set the resource client
 $GLOBALS['RESOURCE'] = Injector::create([new AppModule('test')])->getInstance('\BEAR\Resource\ResourceInterface');

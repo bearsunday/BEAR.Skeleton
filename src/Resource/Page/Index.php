0e5f77cb811fce0532575187a5eea43e3045c0c6
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-12-12 05:18:58 +0900

    minimum greeting resource

--- src/Resource/Page/Index.php
+++ src/Resource/Page/Index.php
@@ -3,19 +3,9 @@
 namespace BEAR\Skeleton\Resource\Page;
 
 use BEAR\Resource\ResourceObject;
-use BEAR\Sunday\Inject\ResourceInject;
 
 class Index extends ResourceObject
 {
-    use ResourceInject;
-
-    /**
-     * @var array
-     */
-    public $body = [
-        'greeting' =>  ''
-    ];
-
     public function onGet($name = 'BEAR.Sunday')
     {
         $this['greeting'] = 'Hello ' . $name;

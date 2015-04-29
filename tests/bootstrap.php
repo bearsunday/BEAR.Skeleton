65959a0fa3bf659c3c9cc1c949009cbeb3ed3389
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-04-30 01:20:13 +0900

    update app module install

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,10 @@
 <?php
 
-use BEAR\Resource\ResourceInterface;
 use BEAR\Skeleton;
 use BEAR\Skeleton\Module\AppModule;
+use BEAR\AppMeta\AppMeta;
+use BEAR\Package\AppMetaModule;
+use BEAR\Resource\ResourceInterface;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Ray\Di\Injector;
 
@@ -19,4 +21,6 @@ $_ENV['TEST_DIR'] = __DIR__;
 $_ENV['TMP_DIR'] = __DIR__ . '/tmp';
 
 // set the resource client
-$GLOBALS['RESOURCE'] = (new Injector(new AppModule, __DIR__ . '/tmp'))->getInstance(ResourceInterface::class);
+// set the resource client
+$module = new AppModule(new AppMetaModule(new AppMeta('BEAR\Skeleton')));
+$GLOBALS['RESOURCE'] = (new Injector($module, __DIR__ . $_ENV['TMP_DIR']))->getInstance(ResourceInterface::class);

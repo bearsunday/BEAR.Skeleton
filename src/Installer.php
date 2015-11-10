9b3d9057784f6c71fdb020373f6f4b9db6233c47
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-11-10 21:07:06 +0900

    vendor/package interactive input

--- src/Installer.php
+++ src/Installer.php
@@ -2,24 +2,43 @@
 
 namespace BEAR\Skeleton;
 
+use Composer\Factory;
+use Composer\IO\IOInterface;
+use Composer\Json\JsonFile;
 use Composer\Script\Event;
 
-/**
- * This file is part of the BEAR.Skeleton package
- *
- * @license http://opensource.org/licenses/bsd-license.php BSD
- */
 class Installer
 {
+    public static function preInstall(Event $event)
+    {
+        $io = $event->getIO();
+        $vendor = self::ask($io, 'What is the vendor name ?', 'Vendor');
+        $package = self::ask($io, 'What is the pacakge name ?', 'Package');
+
+        $composerFile = Factory::getComposerFile();
+        $json = new JsonFile($composerFile);
+        $composerDefinition = $json->read();
+        $composerDefinition['extra']['package'] = [$vendor, $package];
+        $composerDefinition['license'] = 'proprietary';
+        unset($composerDefinition['autoload']['files']);
+        unset($composerDefinition['scripts']);
+        unset($composerDefinition['require-dev']['composer/composer']);
+        $composerDefinition['name'] = sprintf('%s/%s', strtolower($vendor), strtolower($package));
+        $composerDefinition['description'] = '';
+        $composerDefinition['license'] = 'proprietary';
+        $composerDefinition['autoload']['psr-4'] = ["{$vendor}\\{$package}\\" => "src/"];
+        // Update composer definition
+        $json->write($composerDefinition);
+        $io->write("<info>comoser.json is created.\n</info>");
+    }
+
     public static function postInstall(Event $event = null)
     {
+        $composerFile = Factory::getComposerFile();
+        $json = new JsonFile($composerFile);
+        $composerDefinition = $json->read();
+        list($vendorName, $packageName) = $composerDefinition['extra']['package'];
         $skeletonRoot = dirname(__DIR__);
-        $folderName = (new \SplFileInfo($skeletonRoot))->getFilename();
-        $appNameRegex = '/^[A-Za-z0-9]+\.[A-Za-z0-9]+$/';
-        if (! preg_match($appNameRegex, $folderName)) {
-            throw new \LogicException('Application name must be in the format "Vendor.Application".');
-        }
-        list($vendorName, $packageName) = explode('.', $folderName);
         $jobChmod = function (\SplFileInfo $file) {
             chmod($file, 0777);
         };
@@ -31,36 +50,35 @@ class Installer
             $contents = file_get_contents($file);
             $contents = str_replace('BEAR.Skeleton', "{$vendorName}.{$packageName}", $contents);
             $contents = str_replace('BEAR\Skeleton', "{$vendorName}\\{$packageName}", $contents);
-            $contents = str_replace('{package_name}', strtolower("{$vendorName}/{$packageName}"), $contents);
+            $contents = str_replace('bear/greeting', strtolower("{$vendorName}/{$packageName}"), $contents);
             file_put_contents($file, $contents);
         };
-
         // chmod
         self::recursiveJob("{$skeletonRoot}/var/tmp", $jobChmod);
-
+        self::recursiveJob("{$skeletonRoot}/var/log", $jobChmod);
         // rename file contents
         self::recursiveJob("{$skeletonRoot}", $jobRename);
-
-        // composer.json
-        unlink("{$skeletonRoot}/composer.json");
-        unlink("{$skeletonRoot}/composer.lock");
-        rename("{$skeletonRoot}/composer.json.dist", "{$skeletonRoot}/composer.json");
-        $composerJson = file_get_contents("{$skeletonRoot}/composer.json");
-        $packageNameComposerJson = str_replace('bear/skeleton', strtolower("{$vendorName}/{$packageName}"), $composerJson);
-        $packageNameComposerJson = str_replace('BEAR\\\\Skeleton\\\\', "{$vendorName}\\\\{$packageName}\\\\", $packageNameComposerJson);
-        file_put_contents("{$skeletonRoot}/composer.json", $packageNameComposerJson);
-
-        // remove ./vendor
-        $unlink = function ($dir) use (&$unlink) {
-            foreach(scandir($dir) as $file) {
-                if ('.' === $file || '..' === $file) continue;
-                is_dir("$dir/$file") ? $unlink("$dir/$file") : unlink("$dir/$file");
-            }
-            rmdir($dir);
-        };
-        $unlink($skeletonRoot . '/vendor');
+        // renew composer.json
+        $composerFile = Factory::getComposerFile();
+        $json = new JsonFile($composerFile);
+        $composerDefinition = $json->read();
+        unset($composerDefinition['extra']);
+        $json->write($composerDefinition);
+        // remove installer files
         unlink($skeletonRoot . '/README.md');
         unlink(__FILE__);
+        $event->getIO()->write("<info>Thank you for using BEAR.Sunday !\n</info>");
+    }
+
+    private static function ask(IOInterface $io, $question, $item)
+    {
+        $ask = [
+            sprintf("\n<question>%s</question>\n", $question)
+        ];
+        $answer = $io->ask($ask, []);
+        $io->write("<info>{$item}:{$answer}</info>");
+
+        return $answer;
     }
 
     /**

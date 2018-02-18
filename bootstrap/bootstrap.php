0435fa14059c2156f8d3087b666f7da2d1bd4898
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-02-18 20:36:10 +0900

    remove empty line

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,17 +1,13 @@
 <?php
-
 use BEAR\Package\Bootstrap;
-use BEAR\Resource\ResourceObject;
 
 require dirname(__DIR__) . '/autoload.php';
-
 /* @global string $context */
 $app = (new Bootstrap)->getApp('BEAR\Skeleton', $context, dirname(__DIR__));
 $request = $app->router->match($GLOBALS, $_SERVER);
-
 try {
     $page = $app->resource->{$request->method}->uri($request->path)($request->query);
-    /* @var $page ResourceObject */
+    /* @var $page \BEAR\Resource\ResourceObject */
     $page->transfer($app->responder, $_SERVER);
     exit(0);
 } catch (\Exception $e) {
--- bootstrap/web.php
+++ bootstrap/web.php
@@ -1,4 +1,3 @@
 <?php
-
 $context = PHP_SAPI === 'cli' ? 'cli-hal-app' : 'hal-app';
 require __DIR__ . '/bootstrap.php';

f07429a292c3a514bac11cb43fef99401e470f19
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-08-13 02:37:44 +0900

    chmod 775 for tmp

--- bin/setup.php
+++ bin/setup.php
@@ -2,5 +2,5 @@
 
 chdir(dirname(__DIR__));
 passthru('rm -rf var/tmp/*');
-passthru('chmod 777 var/tmp');
-passthru('chmod 777 var/log');
+passthru('chmod 775 var/tmp');
+passthru('chmod 775 var/log');

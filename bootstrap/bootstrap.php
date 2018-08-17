1d7ede8ed1ae7aa523ab0e124877839eb6483fdb
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-08-17 21:28:34 +0900

    replace Bootstrap to App in bootstrap.php

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,16 +1,16 @@
 <?php
-use BEAR\Package\Bootstrap;
+use BEAR\Package\App;
 use BEAR\Resource\ResourceObject;
 
 require dirname(__DIR__) . '/autoload.php';
 
 /* @global string $context */
-$app = (new Bootstrap)->getApp('BEAR\Skeleton', $context, dirname(__DIR__));
+$app = (new App)('BEAR\Skeleton', $context, dirname(__DIR__) . '.env');
 $request = $app->router->match($GLOBALS, $_SERVER);
 try {
-    $page = $app->resource->{$request->method}->uri($request->path)($request->query);
-    /* @var ResourceObject $page */
-    $page->transfer($app->responder, $_SERVER);
+    $response = $app->resource->{$request->method}->uri($request->path)($request->query);
+    /* @var ResourceObject $response */
+    $response->transfer($app->responder, $_SERVER);
     exit(0);
 } catch (\Exception $e) {
     $app->error->handle($e, $request)->transfer();

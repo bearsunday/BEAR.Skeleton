23c651d6af7ee4bbb76ad1b33d5c9274173bca31
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-08-18 09:36:05 +0900

    change bootstrap anonymous function

--- bootstrap/api.php
+++ bootstrap/api.php
@@ -1,3 +1,3 @@
 <?php
-$context = PHP_SAPI === 'cli' ? 'cli-hal-api-app' : 'hal-api-app';
-require __DIR__ . '/bootstrap.php';
+require dirname(__DIR__) . '/autoload.php';
+exit((require __DIR__ . '/bootstrap.php')(PHP_SAPI === 'cli' ? 'cli-hal-api-app' : 'hal-api-app'));
--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,18 +1,17 @@
-<?php
+<?php declare(strict_types=1);
 use BEAR\Package\Bootstrap;
 use BEAR\Resource\ResourceObject;
 
-require dirname(__DIR__) . '/autoload.php';
-
-/* @global string $context */
-$app = (new Bootstrap)->getApp('BEAR\Skeleton', $context, dirname(__DIR__));
-$request = $app->router->match($GLOBALS, $_SERVER);
-try {
-    $page = $app->resource->{$request->method}->uri($request->path)($request->query);
-    /* @var ResourceObject $page */
-    $page->transfer($app->responder, $_SERVER);
-    exit(0);
-} catch (\Exception $e) {
-    $app->error->handle($e, $request)->transfer();
-    exit(1);
-}
+return function (string $context) : int {
+    $app = (new Bootstrap)->getApp('BEAR\Skeleton', $context, dirname(__DIR__));
+    $request = $app->router->match($GLOBALS, $_SERVER);
+    try {
+        $response = $app->resource->{$request->method}->uri($request->path)($request->query);
+        /* @var ResourceObject $response */
+        $response->transfer($app->responder, $_SERVER);
+        return 0;
+    } catch (\Exception $e) {
+        $app->error->handle($e, $request)->transfer();
+        return 1;
+    }
+};

2e84125a6973498dd4a305dd855a593fa0dbf06d
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-10-15 18:20:42 +0900

    make HTTP cache the first class citizen

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,16 @@
 <?php
 use BEAR\Package\Bootstrap;
 use BEAR\Resource\ResourceObject;
+use BEAR\Skeleton\Module\App;
 
 return function (string $context, string $name = 'BEAR\Skeleton') : int {
     $app = (new Bootstrap)->getApp($name, $context, __DIR__);
+    /* @var App $app */
+    if ($app->httpCache->isNotModified($_SERVER)) {
+        $app->httpCache->transfer();
+
+        return 0;
+    }
     $request = $app->router->match($GLOBALS, $_SERVER);
     try {
         $response = $app->resource->{$request->method}->uri($request->path)($request->query);

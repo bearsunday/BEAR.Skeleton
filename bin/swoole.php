569fea1ad65be1d3898d540e087cf0eca9d4c054
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-11-20 22:36:01 +0900

    add comment

--- bin/swoole.php
+++ bin/swoole.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Bootstarp script for Swoole
+ *
+ * @See http://bearsunday.github.io/manuals/1.0/en/swoole.html
+ */
 require dirname(__DIR__) . '/autoload.php';
 exit((require dirname(__DIR__) . '/vendor/bear/swoole/bootstrap.php')(
     'prod-app',       // context

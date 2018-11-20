a4aa542aade41f2c0046526ea27fb610618f6675
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-11-21 00:24:52 +0900

    fix cs

--- bin/swoole.php
+++ bin/swoole.php
@@ -1,5 +1,4 @@
 <?php
-
 // @See http://bearsunday.github.io/manuals/1.0/en/swoole.html
 require dirname(__DIR__) . '/autoload.php';
 exit((require dirname(__DIR__) . '/vendor/bear/swoole/bootstrap.php')(

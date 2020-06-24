cd03155144df46379a956ec6ce9e921aaa4725fa
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-06-24 14:20:40 +0900

    add hypermedia test

---
+++ tests/Http/WorkflowTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BEAR\Skeleton\Http;
+
+use BEAR\Dev\Http\BuiltinServerStartTrait;
+use BEAR\Resource\ResourceObject;
+use BEAR\Skeleton\Hypermedia\WorkflowTest as Workflow;
+use BEAR\Skeleton\Injector;
+
+class WorkflowTest extends Workflow
+{
+    use BuiltinServerStartTrait;
+
+    protected function setUp() : void
+    {
+        $_SERVER['Authorization'] = '_secret_token_';
+        $this->resource = $this->getHttpResourceClient(Injector::getInstance('app'), __CLASS__);
+    }
+
+    public function testIndex() : ResourceObject
+    {
+        $index = $this->resource->get($this->httpHost . '/');
+        $this->assertSame(200, $index->code);
+
+        return $index;
+    }
+}

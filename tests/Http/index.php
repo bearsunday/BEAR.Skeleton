5195023a79b08599e36ee9d10dbcff60551bc72f
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-06-27 03:20:42 +0900

    soothe psalm

--- tests/Http/index.php
+++ tests/Http/index.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 use BEAR\Skeleton\Bootstrap;
 
 require dirname(__DIR__, 2) . '/vendor/autoload.php';
-exit((new Bootstrap)('hal-app', $GLOBALS, $_SERVER));
+exit((new Bootstrap)('hal-app', $GLOBALS, $_SERVER)); // @phpstan-ignore-line

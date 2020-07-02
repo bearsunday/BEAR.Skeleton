4964493b12adf6c75db48034be434ec900244b91
koriym <akihito.koriyama@gmail.com>
2020-07-02 15:43:20 +0900

    cateh Error in the bootstrap

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -6,12 +6,15 @@ namespace BEAR\Skeleton;
 
 use BEAR\Skeleton\Module\App;
 use BEAR\Sunday\Extension\Application\AppInterface;
-use BEAR\Sunday\Extension\Router\RouterInterface;
+use Error;
+use ErrorException;
 use Exception;
 
+use function assert;
+
 /**
- * @psalm-import-type Globals from RouterInterface
- * @psalm-import-type Server from RouterInterface
+ * @psalm-import-type Globals from \BEAR\Sunday\Extension\Router\RouterInterface
+ * @psalm-import-type Server from \BEAR\Sunday\Extension\Router\RouterInterface
  */
 final class Bootstrap
 {
@@ -23,15 +26,16 @@ final class Bootstrap
      *
      * @return 0|1
      */
-    public function __invoke(string $context, array $globals, array $server) : int
+    public function __invoke(string $context, array $globals, array $server): int
     {
-        $app = (Injector::getInstance($context))->getInstance(AppInterface::class);
+        $app = Injector::getInstance($context)->getInstance(AppInterface::class);
         assert($app instanceof App);
         if ($app->httpCache->isNotModified($server)) {
             $app->httpCache->transfer();
 
             return 0;
         }
+
         $request = $app->router->match($globals, $server);
         try {
             $response = $app->resource->{$request->method}->uri($request->path)($request->query);
@@ -40,6 +44,11 @@ final class Bootstrap
             return 0;
         } catch (Exception $e) {
             $app->error->handle($e, $request)->transfer();
+
+            return 1;
+        } catch (Error $e) {
+            $error = new ErrorException($e->getMessage(), $e->getCode(), E_ERROR, $e->getFile(), $e->getLine());
+            $app->error->handle($error, $request)->transfer();
 
             return 1;
         }

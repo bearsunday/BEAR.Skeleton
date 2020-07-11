d7dad68cf868efef2eeae76cc8642beeab5a4106
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-07-11 16:40:06 +0900

    Inject ThrowableHandlerInterface to App root object

--- src/Module/App.php
+++ src/Module/App.php
@@ -4,8 +4,43 @@ declare(strict_types=1);
 
 namespace BEAR\Skeleton\Module;
 
-use BEAR\Sunday\Extension\Application\AbstractApp;
+use BEAR\Resource\ResourceInterface;
+use BEAR\Sunday\Extension\Application\AppInterface;
+use BEAR\Sunday\Extension\Error\ErrorInterface;
+use BEAR\Sunday\Extension\Error\ThrowableHandlerInterface;
+use BEAR\Sunday\Extension\Router\RouterInterface;
+use BEAR\Sunday\Extension\Transfer\HttpCacheInterface;
+use BEAR\Sunday\Extension\Transfer\TransferInterface;
 
-class App extends AbstractApp
+final class App implements AppInterface
 {
+    /** @var HttpCacheInterface */
+    public $httpCache;
+
+    /** @var RouterInterface */
+    public $router;
+
+    /** @var TransferInterface */
+    public $responder;
+
+    /** @var ResourceInterface */
+    public $resource;
+
+    /** @var ThrowableHandlerInterface */
+    public $throwableHandler;
+
+    public function __construct(
+        HttpCacheInterface $httpCache,
+        RouterInterface $router,
+        TransferInterface $responder,
+        ResourceInterface $resource,
+        ThrowableHandlerInterface $throwableHandler,
+        ErrorInterface $error
+    ) {
+        $this->httpCache = $httpCache;
+        $this->router = $router;
+        $this->responder = $responder;
+        $this->resource = $resource;
+        $this->throwableHandler = $throwableHandler;
+    }
 }

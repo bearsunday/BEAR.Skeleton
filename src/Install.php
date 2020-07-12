e2c0f7bf7c655296da7d1be673b0901956d08582
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-07-12 10:31:15 +0900

    Improve camel2dashed
    "bear" not "b-e-a-r" when BEAR is given.

--- src/Install.php
+++ src/Install.php
@@ -115,7 +115,7 @@ final class Install
 
     private function camel2dashed(string $name): string
     {
-        return strtolower((string) preg_replace('/([a-zA-Z])(?=[A-Z])/', '$1-', $name));
+        return strtolower(preg_replace('/([a-zA-Z])(?=[A-Z])/', '$1-', $name));
     }
 
     private function modifyFiles(string $vendor, string $project): void

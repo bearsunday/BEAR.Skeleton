1e95f97e58fb6548b11496f8e1eade425d4d1f2e
koriym <akihito.koriyama@gmail.com>
2021-05-24 12:08:05 +0900

    Update http test

--- tests/Http/WorkflowTest.php
+++ tests/Http/WorkflowTest.php
@@ -4,26 +4,13 @@ declare(strict_types=1);
 
 namespace BEAR\Skeleton\Http;
 
-use BEAR\Dev\Http\BuiltinServerStartTrait;
-use BEAR\Resource\ResourceObject;
+use BEAR\Dev\Http\HttpResource;
 use BEAR\Skeleton\Hypermedia\WorkflowTest as Workflow;
-use BEAR\Skeleton\Injector;
 
 class WorkflowTest extends Workflow
 {
-    use BuiltinServerStartTrait;
-
     protected function setUp(): void
     {
-        $_SERVER['Authorization'] = '_secret_token_';
-        $this->resource = $this->getHttpResourceClient(Injector::getInstance('app'), self::class);
-    }
-
-    public function testIndex(): ResourceObject
-    {
-        $index = $this->resource->get($this->httpHost . '/');
-        $this->assertSame(200, $index->code);
-
-        return $index;
+        $this->resource = new HttpResource('127.0.0.1:8080', __DIR__ . '/index.php', __DIR__ . '/log/workflow.log');
     }
 }

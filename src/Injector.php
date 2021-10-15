fd8fa29750673bbd965dac41e2ce90ba7bb7877f
koriym <akihito.koriyama@gmail.com>
2021-10-16 00:36:27 +0900

    Add Injector::getOverrideInstance()

--- src/Injector.php
+++ src/Injector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace BEAR\Skeleton;
 
 use BEAR\Package\Injector as PackageInjector;
+use Ray\Di\AbstractModule;
 use Ray\Di\InjectorInterface;
 
 use function dirname;
@@ -24,5 +25,10 @@ final class Injector
     public static function getInstance(string $context): InjectorInterface
     {
         return PackageInjector::getInstance(__NAMESPACE__, $context, dirname(__DIR__));
+    }
+
+    public static function getOverrideInstance(string $context, AbstractModule $overrideModule): InjectorInterface
+    {
+        return PackageInjector::getOverrideInstance(__NAMESPACE__, $context, dirname(__DIR__), $overrideModule);
     }
 }

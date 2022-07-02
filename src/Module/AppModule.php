8490f47dbf49e9beb1c83a29dff5ba2dc65bc938
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-07-03 03:54:17 +0900

    Cleanup

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace BEAR\Skeleton\Module;
 
-use BEAR\Dotenv\Dotenv;
 use BEAR\Package\AbstractAppModule;
 use BEAR\Package\PackageModule;
-
-use function dirname;
 
 class AppModule extends AbstractAppModule
 {

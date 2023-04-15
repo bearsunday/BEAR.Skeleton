ab65682fbb178a0e57b0c87d2598c466189f9266
Akihito Koriyama <akihito.koriyama@gmail.com>
2023-04-16 01:52:04 +0900

    Revert "Remove bear/dotenv"
    This reverts commit 1ba48039053513a2d8a6f18da7d84072573078ab.

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -2,15 +2,18 @@
 
 declare(strict_types=1);
 
-namespace BEAR\Skeleton\Module;
+namespace MyVendor\MyProject\Module;
 
+use BEAR\Dotenv\Dotenv;
 use BEAR\Package\AbstractAppModule;
 use BEAR\Package\PackageModule;
+use function dirname;
 
 class AppModule extends AbstractAppModule
 {
     protected function configure(): void
     {
+        (new Dotenv())->load(dirname(__DIR__, 2));
         $this->install(new PackageModule());
     }
 }

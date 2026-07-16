e49db7e3109e6780a5268254f9f38caa377ce897
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-07-16 23:54:53 +0900

    Update ray/di to 2.22.0 and complete WorkflowTest migration
    - Update bear/api-doc to ^1.11 (fix ^1.11s typo) and bear/devtools to ^1.4
    - Remove roave/security-advisories (superseded by bear/security)
    - Migrate Hypermedia WorkflowTest to BEAR\Dev\Http\AbstractWorkflowTest
      using newResource() and follow() helper
    - Align Http WorkflowTest with the newResource() contract
    - Add name:foo Link to Index for a meaningful testRelFoo hypermedia test

--- tests/Http/WorkflowTest.php
+++ tests/Http/WorkflowTest.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace BEAR\Skeleton\Http;
 
 use BEAR\Dev\Http\HttpResource;
+use BEAR\Resource\ResourceInterface;
 use BEAR\Skeleton\Hypermedia\WorkflowTest as Workflow;
 
 class WorkflowTest extends Workflow
 {
-    protected function setUp(): void
+    protected function newResource(): ResourceInterface
     {
-        $this->resource = new HttpResource('127.0.0.1:8080', __DIR__ . '/index.php', __DIR__ . '/log/workflow.log');
+        return new HttpResource('127.0.0.1:8080', __DIR__ . '/index.php', __DIR__ . '/log/workflow.log');
     }
 }
